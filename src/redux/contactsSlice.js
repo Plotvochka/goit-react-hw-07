7f192ba3da1626c89528373267f2d4a209f1ba3f
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-09-24 16:27:09 +0300

    fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -15,7 +15,7 @@ const handleRejected = (state, action) => {
 };
 
 const sliceContacts = createSlice({
-  name: "contacts",
+  name: "Contacts",
   initialState: INITIAL_STATE,
   extraReducers: (builder) => {
     builder
